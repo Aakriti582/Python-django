92fa480a588d0987b8da6bd8657b5fd870bfa6be
Script Shadow <adhikarikamu24@gmail.com>
2026-03-18 19:13:35 +0545

    updated files

---
+++ Students/kamala/script.js
@@ -0,0 +1,12 @@
+let number1 = 10
+let number2 = 20
+// console.log(number1+ number2)
+let operator= "*";
+let result;
+if(operator == "+"){
+    result = number1 + number2;
+}
+else if (operator == "*"){
+    result = number1* number2;
+}
+console.log("Print the sum",result);
